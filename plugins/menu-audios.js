8c4fe39945ec9c05e42550202c7e8f0635f2de80
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-08-30 08:56:14 -0500

    Update menu-audios.js

--- plugins/menu-audios.js
+++ plugins/menu-audios.js
@@ -84,6 +84,12 @@ let str = `
 ° ඬ⃟🔊 _Épico_
 
 `.trim()
+conn.sendHydrated(m.chat, str, wm, pp, 'https://github.com/BrunoSobrino/TheMystic-Bot-MD', '𝙶𝙸𝚃𝙷𝚄𝙱', null, null, [
+['𝙼𝙴𝙽𝚄 𝙿𝚁𝙸𝙽𝙲𝙸𝙿𝙰𝙻', '/menu']
+], m)
+}}
 handler.help = ['menu2', 'help2', '?2', 'menuaudios']
+handler.tags = ['main']
 handler.command = /^(menu2|audios|menú2|memu2|menuaudio|menuaudios|memuaudios|memuaudio|audios|audio)$/i
+handler.fail = null
 export default  handler
