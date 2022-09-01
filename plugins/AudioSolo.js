cb07de734b5d118076f1a90e4547a4f5e45d11a6
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-09-01 02:27:25 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/AudioSolo.js
+++ plugins/AudioSolo.js
@@ -6,6 +6,6 @@ let vn = './media/Nose.mp3'
 conn.sendPresenceUpdate('recording', m.chat)
 conn.sendFile(m.chat, vn, 'Nose.mp3', null, m, true, { type: 'audioMessage', ptt: true })
 }
-handler.customPrefix = /nose|Nose|no se|no sé|No sé|No se|No Se|No Sé|NOSE/
+handler.customPrefix = /nose|Nose|no se|no sé|No sé|No se|No Se|No Sé/
 handler.command = /^(nose|Nose|no se|no sé|No sé|No se|No Se|No Sé|NOSE?$)/
 export default handler
