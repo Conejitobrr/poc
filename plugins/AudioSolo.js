703abbd76bc6bad155fa39881326e1033a05664e
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-09-01 02:31:02 -0500

    Update AudioSolo.js

--- plugins/AudioSolo.js
+++ plugins/AudioSolo.js
@@ -1,11 +1,15 @@
 import util from 'util'
 import path from 'path'
+
 let handler = async (m, { conn }) => {
 if (!db.data.chats[m.chat].audios && m.isGroup) throw 0
 let vn = './media/Nose.mp3'
-conn.sendPresenceUpdate('recording', m.chat)
-conn.sendFile(m.chat, vn, 'Nose.mp3', null, m, true, { type: 'audioMessage', ptt: true })
+conn.sendFile(m.chat, vn, 'Nose.mp3', null, m, true, {
+type: 'audioMessage', 
+ptt: true 
+})
 }
-handler.customPrefix = /nose|Nose|no se|no sé|No sé|No se|No Se|No Sé/
-handler.command = /^(nose|Nose|no se|no sé|No sé|No se|No Se|No Sé|NOSE?$)/
+handler.command = /^(nose|Nose|no se|no sé|No sé|No se|No Se|No Sé|NOSE)$/i
+handler.fail = null
+handler.exp = 100
 export default handler
