eb9147a9a16fa86c342f4f377f043eb0a970cc36
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-09-01 02:36:34 -0500

    Update Audios.js

--- plugins/Audios.js
+++ plugins/Audios.js
@@ -137,6 +137,11 @@ if (chat.audios && m.text.match(/(something|Something)/gi)) {
 let vn = './media/Something.mp3'
 this.sendPresenceUpdate('recording', m.chat)   
 this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
+  
+if (chat.audios && m.text.match(/(nose|Nose|no se|no sé|No sé|No se|No Se|No Sé)/gi)) {
+let vn = './media/Something.mp3'
+this.sendPresenceUpdate('recording', m.chat)   
+this.sendFile(m.chat, vn, 'error.mp3', null, m, true, {type: 'audioMessage', ptt: true})}
  
 return !0 }
 export default handler
