944b6406c96ce32232daf3a80770c21004a0bdfd
Conejitobrr <110344521+Conejitobrr@users.noreply.github.com>
2022-09-15 15:58:38 -0500

    🧿 𝚃𝚑𝚎 𝙼𝚢𝚜𝚝𝚒𝚌 - 𝙱𝚘𝚝 🔮

--- plugins/_antiArab.js
+++ plugins/_antiArab.js
@@ -6,7 +6,7 @@ let chat = global.db.data.chats[m.chat]
 
 if (isBotAdmin && chat.antiArab) {
 		
-if (m.sender.startsWith('212' || '212'|| '312')) {
+if (m.sender.startsWith('212' || '212'|| '923')) {
 global.db.data.users[m.sender].banned = true
 m.reply(`✳️ Anti árabes está activo para evitar spam\n\nHasta la próxima`)
 await conn.groupParticipantsUpdate(m.chat, [m.sender], 'remove')}
